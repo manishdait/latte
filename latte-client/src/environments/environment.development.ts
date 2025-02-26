0ce293867977ce1f1710d6208a342263e48c99e7
Manish Dait <daitmanish88@gmail.com>
2025-02-26 11:23:36 +0530

    version 0.0.1

--- latte-client/src/environments/environment.development.ts
+++ latte-client/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  VERSION: '0.0.1-snap',
+  VERSION: '0.0.1',
   API_ENDPOINT: 'http://localhost:8080/latte-api/v1'
 };
--- latte-client/src/environments/environment.ts
+++ latte-client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  VERSION: '0.0.1-snap',
+  VERSION: '0.0.1',
   API_ENDPOINT: '/latte-api/v1'
 };
