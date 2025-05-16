a5aa5692a4079b8e1bc5d87112a7c1e5727cb1fb
Manish Dait <daitmanish88@gmail.com>
2025-05-16 13:56:07 +0530

    update enviroments

--- latte-client/src/environments/environment.ts
+++ latte-client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   PROFILE: 'prod',
-  VERSION: '0.0.2',
+  VERSION: 'v0.0.2',
   API_ENDPOINT: 'https://aesthetic-cathee-manishdait-9d4db02e.koyeb.app/latte-api/v1',
   WEBSOCKET_ENDPONT: 'https://aesthetic-cathee-manishdait-9d4db02e.koyeb.app/ws'
 };
